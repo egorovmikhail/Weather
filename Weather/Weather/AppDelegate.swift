91f2d9c4b0c0f1c50298cf346d69093e890b069a
Михаил <40540053+egorovmikhail@users.noreply.github.com>
2020-12-15 23:01:37 +1100

    Add files via upload

---
+++ Weather/Weather/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Михаил Егоров on 15.12.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
+
