f18df008779809aa0f0272f4f6552125604261e2
MikhailEgorov <egorovmikhail@bk.ru>
2020-12-17 02:29:36 +1100

    Добавил библиотеки Alamofire и Realm

--- Weather/App/Weather/WeatherViewController.swift
+++ Weather/App/Weather/WeatherViewController.swift
@@ -8,23 +8,55 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
-  @IBOutlet weak var collectionView: UICollectionView!
+  
+  let weatherService = WeatherService()
+  
+  @IBOutlet weak var collectionView: UICollectionView! {
+    didSet {
+      collectionView.dataSource = self
+      collectionView.delegate = self
+    }
+  }
   
   override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
+    weatherService.loadWeatherData(city: "Moscow")
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
 
-    }
+extension WeatherViewController: UICollectionViewDataSource {
+  
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+    cell.weather.text = "30 C"
+    cell.time.text = "30.08.2017 18:00"
+    return cell
+  }
+  
+}
+
+extension WeatherViewController: UICollectionViewDelegate {
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
 }
---
+++ Weather/App/WeatherService.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherService.swift
+//  Weather
+//
+//  Created by Михаил Егоров on 17.12.2020.
+//
+
+import Foundation
+import Alamofire
+
+class WeatherService {
+  // базовый URL сервиса
+  let baseUrl = "http://api.openweathermap.org"
+  // ключ для доступа к сервису
+  let apiKey = "8eb2bf7c277416368b75c686b2484892"
+  
+  // метод для загрузки данных, в качестве аргументов получает город
+  func loadWeatherData(city: String){
+    
+    // путь для получения погоды за 5 дней
+    let path = "/data/2.5/forecast"
+    // параметры, город, единицы измерения градусы, ключ для доступа к сервису
+    let parameters: Parameters = [
+      "q": city,
+      "units": "metric",
+      "appid": apiKey
+    ]
+    
+    // составляем URL из базового адреса сервиса и конкретного пути к ресурсу
+    let url = baseUrl + path
+    
+    // делаем запрос
+    
+    
+    
+    AF.request(url, method: .get, parameters: parameters).responseJSON { repsonse in
+      print(repsonse.value as Any)
+    }
+    
+  }
+}
+
